3a8efc5c334e4088f760325fc7337d234b8cdefd
xmdhs <xmdhss@gmail.com>
2025-08-08 19:58:21 +0800

    Update build-adguard.sh

--- build-adguard.sh
+++ build-adguard.sh
@@ -2,9 +2,9 @@
 
 set -e -o pipefail
 
-wget https://github.com/SagerNet/sing-box/releases/download/v1.11.0/sing-box-1.11.0-linux-amd64.tar.gz
-tar -zxvf sing-box-1.11.0-linux-amd64.tar.gz
+wget https://github.com/SagerNet/sing-box/releases/download/v1.12.0/sing-box-1.12.0-linux-amd64.tar.gz
+tar -zxvf sing-box-1.12.0-linux-amd64.tar.gz
 
 curl -fsSL https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt -o filter.txt
 
-sing-box-1.11.0-linux-amd64/sing-box rule-set convert --type adguard --output output/AdGuardSDNSFilterSingBox.srs filter.txt
+sing-box-1.12.0-linux-amd64/sing-box rule-set convert --type adguard --output output/AdGuardSDNSFilterSingBox.srs filter.txt
